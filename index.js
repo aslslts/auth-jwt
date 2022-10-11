93cc78016eceda99b2d4d57486d5c5657f3c6e26
Aslı Sulutaş <cemree.1402@gmail.com>
2022-10-12 00:09:27 +0300

    Post route added

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const port = 3000;
 
 //Import Router
-const authRoute = require('./routes/auth');
-
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 dotenv.config();
 
 //Connect to the database
 mongoose.connect(process.env.DB_CONNECT, () => {
-    console.log('connect to Db')
+    console.log("connect to Db");
 });
 
 //Middle ware
 app.use(express.json());
 
-
-
-
-
 //Route Middle wares
-app.use('/api/user', authRoute);
+app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
 
-app.listen(3000, () => console.log('Server Up and running'));
+app.listen(port, () => console.log("Server Up and running"));
